416768d6527d8b45541db049b9e27b1b18d08cd1
Sebastian <seba.saporiti@gmail.com>
2023-02-01 23:19:12 -0300

    created chat components

--- src/components/DisplayChat/DisplayChat.jsx
+++ src/components/DisplayChat/DisplayChat.jsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useContext } from "react";
 import { PlayerContext } from "../../contexts/PlayerProvider";
+import { Chat } from "./components/Chat/Chat";
 import { ChatTag } from "./components/ChatTag/ChatTag";
+import { ProfileSection } from "./components/ProfileSection/ProfileSection";
 import { SendMessage } from "./components/SendMessage/SendMessage";
 import styles from "./styles/displayChat.module.scss";
 
@@ -12,12 +14,16 @@ const DisplayChat = () => {
     action.setShowChat(!showChat);
   }, [action, showChat]);
 
-  const handleSendMessage = useCallback(() => {}, []);
+  const handleSendMessage = useCallback((e) => {
+    e.preventDefault();
+  }, []);
 
   return (
     <div className={showChat ? styles.visible : styles.hidden}>
       <ChatTag handleOpenChat={handleOpenChat} />
       <div className={styles.chatContainer}>
+        <ProfileSection />
+        <Chat />
         <SendMessage handleSendMessage={handleSendMessage} />
       </div>
     </div>
